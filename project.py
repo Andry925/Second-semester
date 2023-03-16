9df1ad38b5f663e88afdb66138dee58289d2beaa
Andrew925 <andry925@ukr.net>
2023-03-16 19:44:41 +0200

    Working project with input directory

--- project.py
+++ project.py
@@ -2,9 +2,12 @@
 import os 
 import shutil
 import time
+
 class Parcer:
     def __init__(self):
        self.list_extension = []
+       seconds_in_day = 86400
+       self.day = int(input("Put the time interval in days")) * 86400
        self.directory = input("Put the directore you want to store the file")
        os.chdir(self.directory)
        
@@ -14,24 +17,25 @@ class Parcer:
     def search(self,path,key_for_search):
         self.key_for_search = key_for_search
         self.path = path
-        for iteration in self.key_for_search.split():
-            self.list_extension.append(iteration)
-            self.y = f"mainfolder\{iteration}"
-            os.makedirs(self.y)
+        for extension in self.key_for_search.split():
+            self.list_extension.append(extension)
+            self.mainfolder = f"mainfolder\{extension}"
+            os.makedirs(self.mainfolder)
     
         for adress,dirs,files in os.walk(self.path):
+            self.path_copy = os.path.join(self.directory,"mainfolder")
             for file in files:
-                for second in self.key_for_search.split():
-                    if second in file:
+                for extension_copy in self.key_for_search.split():
+                    if extension_copy in file:
 
                         our_file =  os.path.join(adress,file)
-                        if time.time() - os.path.getctime(our_file) < 2000000:
+                        if time.time() - os.path.getctime(our_file) < self.day and "$" not in our_file:
                             try:
                         
-                                shutil.move(our_file,f"C:\Второй семестр\mainfolder\{second}")
+                                shutil.copy(our_file,os.path.join(self.path_copy,extension_copy))
                                 yield our_file
                             except:
-                                with open("C:\Второй семестр\mainfolder\mistakes.txt",'w') as file_mistake:
+                                with open(os.path.join(self.path_copy,"mistakes.txt"),'w') as file_mistake:
                                     file_mistake.write(our_file)
                             
                            
