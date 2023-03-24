e9e015662314f95fd8ac7acb5742c5d2a0f63ecf
Andrew925 <andry925@ukr.net>
2023-03-24 09:57:33 +0200

    Some more functions and clear-name variables

--- project.py
+++ project.py
@@ -1,54 +1,51 @@
-
+from tkinter import Tk
 import os 
 import shutil
 import time
-class Parcer:
+class File_manager(Tk):
     def __init__(self):
        self.list_extension = []
        seconds_in_day = 86400
-       self.day = int(input("Put the time interval in days")) * 86400
+       self.day = float(input("Put the time interval in days")) * 86400
        self.directory = input("Put the directore you want to store the file")
        os.chdir(self.directory)
        self.extension_folders(input("Put the start"),input("Put the extension"))
-       
-       
-       for file_path in self.search(self.path,self.key_for_search):
-          
-           
+       for file_path in self.search(self.path,self.extension_for_search):
            print(file_path)
         
-    def extension_folders(self,path,key_for_search):
-        self.key_for_search = key_for_search
+    def extension_folders(self,path,extension_for_search):
+        self.extension_for_search = extension_for_search
         self.path = path
-        for extension in self.key_for_search.split():
+        for extension in self.extension_for_search.split():
             self.list_extension.append(extension)
             self.mainfolder = f"mainfolder\{extension}"
             os.makedirs(self.mainfolder)
-        return self.path, self.key_for_search
+        return self.path, self.extension_for_search
 
-        
-    
     def search(self,path,key_for_search):
         for adress,dirs,files in os.walk(self.path):
             self.path_copy = os.path.join(self.directory,"mainfolder")
             for file in files:
-                for extension_copy in self.key_for_search.split():
+                for extension_copy in self.extension_for_search.split():
                     if extension_copy in file:
 
                         our_file =  os.path.join(adress,file)
-                        if time.time() - os.path.getctime(our_file) < self.day and "$" not in our_file:
+                        if time.time() - os.path.getctime(our_file) < self.day and "$"  not in our_file:
                             try:
                         
-                                shutil.copy(our_file,os.path.join(self.path_copy,extension_copy))
+                                shutil.move(our_file,os.path.join(self.path_copy,extension_copy))
                                 yield our_file
                             except:
                                 with open(os.path.join(self.path_copy,"mistakes.txt"),'w') as file_mistake:
                                     file_mistake.write(our_file)
+                               
                             
-                           
-                               
+x = File_manager()
 
-                               
-            
-x = Parcer()
-
+while True:
+    what_to_do = input("Do you want to delete these files ?")
+    if what_to_do == "Yes":
+        shutil.rmtree("mainfolder")
+        break
+    else:
+        break
