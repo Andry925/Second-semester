20875e59b038fc45e75417f8a84c710fc7e38876
Andrew925 <andry925@ukr.net>
2023-04-14 21:54:22 +0300

    Fixing errors

--- project.py
+++ project.py
@@ -36,29 +36,31 @@ class FileManager():
         
 
     def create_full_path(self):
-        self.list_defect = ["$",".lnk",".LNK"]
+        self.list_defect = ["$","lnk","LNK"]
         self.path_copy = os.path.join(self.directory, self.name_folder)
         for adress, dirs, files in os.walk(self.way):
 
             for file in files:
                 for extension_copy in self.extension_for_search.split():
-                    if extension_copy in file and not any(defect in our_file for defect in self.list_defect):
+                    if extension_copy in file and not any(defect in file for defect in self.list_defect):
                         our_file = os.path.join(adress, file)
-                        if time.time() - os.path.getctime(our_file) < self.day:
+                        if time.time() - os.path.getctime(our_file) < self.day and "$" not in our_file:
                            
                             
                             yield our_file
 
     def sort_and_move(self, our_file):
-        for extension in self.list_extension:
-            try:
-                if extension in our_file:
-                    shutil.copy(our_file, os.path.join(self.path_copy, extension))
+        extension_to_copy = our_file.split(".")[-1]
+       
+        try:
+            if extension_to_copy in self.list_extension:
+            
+                shutil.copy(our_file, os.path.join(self.path_copy, extension_to_copy))
                     
 
-            except :
-                with open(os.path.join(self.path_copy, "mistakes.txt"), 'a') as file_mistake:
-                    file_mistake.write(f"{our_file}\n")
+        except :
+            with open(os.path.join(self.path_copy, "mistakes.txt"), 'a') as file_mistake:
+                file_mistake.write(f"{our_file}\n")
 
     def final_decision(self):
         self.what_to_do = input("Do you want to delete these files -")
@@ -94,6 +96,4 @@ class Interface(tkinter.Tk):
         
 root = Interface()
 root.mainloop()
-
-
 
