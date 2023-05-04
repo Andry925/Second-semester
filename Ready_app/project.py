2f84d77acee04a83d4c83e87b46d3ed1d9931931
Andrew925 <andry925@ukr.net>
2023-05-04 22:23:40 +0300

    Working for others

--- Ready_app/project.py
+++ Ready_app/project.py
@@ -8,8 +8,10 @@ list_defect = ["$", ".lnk", ".LNK"]
 
 
 class FileManager():
+    
     def __init__(self):
         self.list_extension = []
+        
         self.name_folder = input("How would you call mainfolder")
         self.docx_information = input("What do you wnat for docx")
         while True:
@@ -34,29 +36,33 @@ class FileManager():
                         for docx_file in self.checking_docx_files(file,self.docx_information):
                         
                             self.sort_and_move(docx_file)
+                            print(f"File is copied {docx_file} and paragraph is {self.count}")
                 except:
                         print(f"Something went wrong with{docx_file}")
                     
 
                
 
-            if ".docx" not in file:
-                self.sort_and_move(file)
-                print(f"File is copied{file}")
+                if ".docx" not in file:
+                    self.sort_and_move(file)
+                    print(f"File is copied{file}")
         self.final_decision()
 
     
     def checking_docx_files(self,our_file,information):
+        self.count = 0
         self.information = information
         
-        try:
-            doc = docx.Document(our_file)
-            for paragraph in doc.paragraphs:
-                text_file = paragraph.text
-                if self.information in text_file:
-                    yield our_file
-        except:
-            print(f"Something went wrong with {our_file}")
+        
+        doc = docx.Document(our_file)
+        for paragraph in doc.paragraphs:
+            text_file = paragraph.text
+            self.count += 1
+            if self.information in text_file:
+                
+                yield our_file,self.count
+       
+           
         
     def extension_folders(self, extension_for_search):
         self.extension_for_search = extension_for_search
