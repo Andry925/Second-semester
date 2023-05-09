536481afda0ccf6cefc8bd93681a3bbaaa860b23
Andrew925 <andry925@ukr.net>
2023-05-09 21:02:17 +0300

    Starting version of interface

--- Ready_app/final_code.py
+++ Ready_app/final_code.py
@@ -1,30 +1,24 @@
-from project import FileManager
 import tkinter
-
+from project import FileManager
 
 class Interface(tkinter.Tk):
     def __init__(self):
         super().__init__()
-        self.window = tkinter.Tk()
-        self.create_window()
-        self.create_widgets()
-    
-    def create_window(self):
-        self.window.title("An interface for my app")
-        self.window.geometry("800x400")
-        self.frame = tkinter.Frame(self.window)
+        self.title("An interface for my app")
+        self.geometry("800x400")
+        self.frame = tkinter.Frame(self)
         self.frame.pack()
+        self.create_widgets()
+        
 
-    
-    
     def create_widgets(self):
-        self.label = tkinter.Label(self.frame,text = "My program")
+        self.label = tkinter.Label(self.frame, text="My program")
         self.label.pack()
         self.textentry = tkinter.Entry(self.frame)
         self.textentry.pack()
-        self.button = tkinter.Button(self.frame,text = "Click me",command = FileManager)
+        self.button = tkinter.Button(self.frame, text="Click me",command=FileManager)
         self.button.pack()
-       
+
         
 root = Interface()
 root.mainloop()
