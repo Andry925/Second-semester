6f3cd7ba09970af262fd68b24ff3011d852b9305
Andrew925 <andry925@ukr.net>
2023-05-11 12:48:38 +0300

    Starting intrface which needs refactoring

--- Gui_Interface/interface.py
+++ Gui_Interface/interface.py
@@ -4,14 +4,14 @@ class Interface(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title("An interface for my app")
-        self.geometry("800x400")
+        self.geometry("300x300")
         self.frame = tkinter.Frame(self)
-        self.frame.pack()
+        self.frame.grid()
         self.create_widgets()
         
 
     def create_widgets(self):
-        
+       
         self.label_mainfolder = tkinter.Label(self.frame, text="How would you call mainfolder")
         self.label_mainfolder.grid()
         self.textentry_mainfolder = tkinter.Entry(self.frame)
@@ -20,6 +20,20 @@ class Interface(tkinter.Tk):
         self.label_days.grid()
         self.textentry_days = tkinter.Entry(self.frame)
         self.textentry_days.grid()
+        self.label_extensions = tkinter.Label(self.frame, text="Put the extensdions you need")
+        self.label_extensions.grid()
+        self.textentry_extensions = tkinter.Entry(self.frame)
+        self.textentry_extensions.grid()
+        self.directory = tkinter.Label(self.frame, text="Put the directory you want to store the file")
+        self.directory.grid()
+        self.directory = tkinter.Entry(self.frame)
+        self.directory.grid()
+        self.path = tkinter.Label(self.frame, text="Put the path to start with")
+        self.path.grid()
+        self.path= tkinter.Entry(self.frame)
+        self.path.grid()
+        self.button = tkinter.Button(self.frame,text = "Run code")
+        self.button.grid()
         
 root = Interface()
 root.mainloop()
