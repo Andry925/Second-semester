fe840dadfb8d3c4dadd9c6c441d4e25b82f987ee
Andrew925 <andry925@ukr.net>
2023-05-11 18:13:03 +0300

    Better organized interface

--- Gui_Interface/interface.py
+++ Gui_Interface/interface.py
@@ -1,5 +1,7 @@
 import tkinter
 
+
+
 class Interface(tkinter.Tk):
     def __init__(self):
         super().__init__()
@@ -7,33 +9,52 @@ class Interface(tkinter.Tk):
         self.geometry("300x300")
         self.frame = tkinter.Frame(self)
         self.frame.grid()
-        self.create_widgets()
+        self.create_labels()
+        self.create_Entry()
+        self.pack_widgets()
+        self.create_button()
+        
         
 
-    def create_widgets(self):
+    def create_labels(self):
        
         self.label_mainfolder = tkinter.Label(self.frame, text="How would you call mainfolder")
-        self.label_mainfolder.grid()
-        self.textentry_mainfolder = tkinter.Entry(self.frame)
-        self.textentry_mainfolder.grid()
         self.label_days = tkinter.Label(self.frame, text="Put the time interval in days ")
-        self.label_days.grid()
+        self.label_extensions = tkinter.Label(self.frame, text="Put the extensions you need")
+        self.label_directory = tkinter.Label(self.frame, text="Put the directory you want to store the file")
+        self.label_path = tkinter.Label(self.frame, text="Put the path to start with")
+       
+        
+    def create_Entry(self):
+        self.textentry_mainfolder = tkinter.Entry(self.frame)
         self.textentry_days = tkinter.Entry(self.frame)
-        self.textentry_days.grid()
-        self.label_extensions = tkinter.Label(self.frame, text="Put the extensdions you need")
-        self.label_extensions.grid()
         self.textentry_extensions = tkinter.Entry(self.frame)
+        self.textentry_directory = tkinter.Entry(self.frame)
+        self.textentry_path= tkinter.Entry(self.frame)
+
+    
+    def pack_widgets(self):
+        self.label_mainfolder.grid()
+        self.textentry_mainfolder.grid()
+        self.label_days.grid()
+        self.textentry_days.grid()
+        self.label_extensions.grid()
         self.textentry_extensions.grid()
-        self.directory = tkinter.Label(self.frame, text="Put the directory you want to store the file")
-        self.directory.grid()
-        self.directory = tkinter.Entry(self.frame)
-        self.directory.grid()
-        self.path = tkinter.Label(self.frame, text="Put the path to start with")
-        self.path.grid()
-        self.path= tkinter.Entry(self.frame)
-        self.path.grid()
+        self.label_directory.grid()
+        self.textentry_directory.grid()
+        self.label_path.grid()
+        self.textentry_path.grid()
+    
+    def create_button(self):
         self.button = tkinter.Button(self.frame,text = "Run code")
         self.button.grid()
-        
+
+
+
+
+
+
+
 root = Interface()
 root.mainloop()
+        
