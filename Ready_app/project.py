7301c5d3ef148d8ac168d15254d1f1116f9076fd
Andrew925 <andry925@ukr.net>
2023-05-20 11:19:29 +0300

    Changing variable names

--- Ready_app/project.py
+++ Ready_app/project.py
@@ -7,14 +7,15 @@ list_defect = ["$", ".lnk", ".LNK"]
 
 
 class FileManager():
+    
     def __init__(self):
-        self.list_extension = []
-        self.name_folder = final_code.get_mainfolder()
+        self.list_needed_extensions = []
+        self.mainfolder_name = final_code.get_mainfolder()
        
         try:
-            self.day = int(
+            self.days = int(
             final_code.get_days()) * seconds_in_day
-            self.extension_for_search = final_code.get_extensions()
+            self.needed_extensions = final_code.get_extensions()
             self.directory = final_code.get_directory()
             os.chdir(str(self.directory))
             self.way = final_code.get_way()
@@ -23,14 +24,17 @@ class FileManager():
                 print("Invalid input of data -")
         self.run_code()
         self.final_decision()
+        
 
-    def extension_folders(self, extension_for_search):
-        self.extension_for_search = extension_for_search
-        for extension in self.extension_for_search.split():
-            self.list_extension.append(extension)
-            self.mainfolder = f"{self.name_folder}\\{extension}"
+    def create_extension_folders(self, needed_extensions):
+        self.needed_extensions = needed_extensions
+        for extension in self.needed_extensions.split():
+            self.list_needed_extensions.append(extension)
+            self.mainfolder = f"{self.mainfolder_name}\\{extension}"
             os.makedirs(self.mainfolder)
             yield extension
+
+
 
 
     def create_full_path(self,extension_copy):
@@ -40,14 +44,14 @@ class FileManager():
 
                 if extension_copy in file:
                      our_file = os.path.join(address, file)
-                     if time.time() - os.path.getctime(our_file) < self.day and not any(
+                     if time.time() - os.path.getctime(our_file) < self.days and not any(
                          defect in our_file for defect in list_defect):
 
                         yield our_file
 
     def sort_and_move(self, our_file):
-        self.path_copy = os.path.join(self.directory, self.name_folder)
-        for extension in self.list_extension:
+        self.path_copy = os.path.join(self.directory, self.mainfolder_name)
+        for extension in self.list_needed_extensions:
             try:
                 if extension in our_file:
                     shutil.copy(
@@ -59,17 +63,17 @@ class FileManager():
                     file_mistake.write(f"{our_file}\n")
     
     def run_code(self):
-         for extension in self.extension_folders(self.extension_for_search):
+         for extension in self.create_extension_folders(self.needed_extensions):
 
             for file in self.create_full_path(extension):
                 self.sort_and_move(file)
-                print(f"File is copied{file}")
+                print(f"File is copied {file}")
 
     
     def final_decision(self):
-        self.what_to_do = input("Do you want to delete these files")
+        self.what_to_do = final_code.get_final_decision()
         if self.what_to_do == "Yes":
-            shutil.rmtree(self.name_folder)
+            shutil.rmtree(self.mainfolder_name)
 
 
 
