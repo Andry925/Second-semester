6e5f9be7ad02783d49f6586d652df596322b8e6d
Andrew925 <andry925@ukr.net>
2023-05-20 10:54:38 +0300

    New interface

--- Gui_Interface/interface.py
+++ Gui_Interface/interface.py
@@ -1,60 +1,13 @@
-import tkinter
+def get_mainfolder():
+    return root.textentry_mainfolder.get()
 
+def get_days():
+    return root.textentry_days.get()
 
+def get_extensions():
+    return root.textentry_extensions.get()
+def get_directory():
+    return root.textentry_directory.get()
+def get_way():
+    return root.textentry_path.get()
 
-class Interface(tkinter.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("An interface for my app")
-        self.geometry("300x300")
-        self.frame = tkinter.Frame(self)
-        self.frame.grid()
-        self.create_labels()
-        self.create_Entry()
-        self.pack_widgets()
-        self.create_button()
-        
-        
-
-    def create_labels(self):
-       
-        self.label_mainfolder = tkinter.Label(self.frame, text="How would you call mainfolder")
-        self.label_days = tkinter.Label(self.frame, text="Put the time interval in days ")
-        self.label_extensions = tkinter.Label(self.frame, text="Put the extensions you need")
-        self.label_directory = tkinter.Label(self.frame, text="Put the directory you want to store the file")
-        self.label_path = tkinter.Label(self.frame, text="Put the path to start with")
-       
-        
-    def create_Entry(self):
-        self.textentry_mainfolder = tkinter.Entry(self.frame)
-        self.textentry_days = tkinter.Entry(self.frame)
-        self.textentry_extensions = tkinter.Entry(self.frame)
-        self.textentry_directory = tkinter.Entry(self.frame)
-        self.textentry_path= tkinter.Entry(self.frame)
-
-    
-    def pack_widgets(self):
-        self.label_mainfolder.grid()
-        self.textentry_mainfolder.grid()
-        self.label_days.grid()
-        self.textentry_days.grid()
-        self.label_extensions.grid()
-        self.textentry_extensions.grid()
-        self.label_directory.grid()
-        self.textentry_directory.grid()
-        self.label_path.grid()
-        self.textentry_path.grid()
-    
-    def create_button(self):
-        self.button = tkinter.Button(self.frame,text = "Run code")
-        self.button.grid()
-
-
-
-
-
-
-
-root = Interface()
-root.mainloop()
-        
