e4b39afd1684d347e3f20885891c9add28141cde
Andrew925 <andry925@ukr.net>
2023-05-22 16:05:58 +0300

    Refactoring final code

--- Ready_app/interface_with_file_manager.py
+++ Ready_app/interface_with_file_manager.py
@@ -2,46 +2,35 @@ import tkinter
 import main
 
 
-
-
-
-
-
 class Interface(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title("An interface for my app")
         self.geometry("300x300")
-        self.resizable(height=False,width=False)
+        self.resizable(height=False, width=False)
         self.frame = tkinter.Frame(self)
         self.frame.grid()
         self.create_labels()
-        self.create_Entry()
+        self.create_entry()
         self.pack_widgets()
         self.final_decision_option()
         self.create_button()
-        
-        
 
     def create_labels(self):
-       
-        self.label_mainfolder = tkinter.Label(self.frame, text="How would you call mainfolder")
-        self.label_days = tkinter.Label(self.frame, text="Put the time interval in days ")
+        self.label_mainfolder = tkinter.Label(
+        self.frame, text="How would you call mainfolder")
+        self.label_days = tkinter.Label(self.frame, text="Put the time interval in days")
         self.label_extensions = tkinter.Label(self.frame, text="Put the extensions you need")
         self.label_directory = tkinter.Label(self.frame, text="Put the directory you want to store the file")
         self.label_path = tkinter.Label(self.frame, text="Put the path to start with")
-       
-       
-        
-    def create_Entry(self):
+
+    def create_entry(self):
         self.textentry_mainfolder = tkinter.Entry(self.frame)
         self.textentry_days = tkinter.Entry(self.frame)
         self.textentry_extensions = tkinter.Entry(self.frame)
         self.textentry_directory = tkinter.Entry(self.frame)
-        self.textentry_path= tkinter.Entry(self.frame)
-        
+        self.textentry_path = tkinter.Entry(self.frame)
 
-    
     def pack_widgets(self):
         self.label_mainfolder.grid()
         self.textentry_mainfolder.grid()
@@ -53,15 +42,13 @@ class Interface(tkinter.Tk):
         self.textentry_directory.grid()
         self.label_path.grid()
         self.textentry_path.grid()
-        
-    
+
     def create_button(self):
-        
-        self.button = tkinter.Button(self.frame,text = "Run code",command = main.FileManager)
+        self.button = tkinter.Button(self.frame, text="Run code", command=main.FileManager)
         self.button.grid()
 
     def final_decision_option(self):
-        self.label_decison = tkinter.Label(self.frame,text ="Do you want to delete these files ?")
+        self.label_decison = tkinter.Label(self.frame, text="Do you want to delete these files ?")
         self.textentry_option = tkinter.Entry(self.frame)
         self.label_decison.grid()
         self.textentry_option.grid()
@@ -73,21 +60,25 @@ root = Interface()
 def get_mainfolder():
     return root.textentry_mainfolder.get()
 
+
 def get_days():
     return root.textentry_days.get()
 
+
 def get_extensions():
     return root.textentry_extensions.get()
+
+
 def get_directory():
     return root.textentry_directory.get()
+
+
 def get_way():
     return root.textentry_path.get()
+
 
 def get_final_decision():
     return root.textentry_option.get()
 
+
 root.mainloop()
-
-
-
-
--- Ready_app/main.py
+++ Ready_app/main.py
@@ -1,30 +1,28 @@
-import interface_with_file_manager
 import os
 import shutil
 import time
+import interface_with_file_manager
 seconds_in_day = 86400
 list_defect = ["$", ".lnk", ".LNK"]
 
 
 class FileManager():
-    
+
     def __init__(self):
         self.list_needed_extensions = []
         self.mainfolder_name = interface_with_file_manager.get_mainfolder()
-       
+
         try:
-            self.days = int(
-            interface_with_file_manager.get_days()) * seconds_in_day
+            self.days = float(interface_with_file_manager.get_days()) * seconds_in_day
             self.needed_extensions = interface_with_file_manager.get_extensions()
             self.directory_to_store_files = interface_with_file_manager.get_directory()
             os.chdir(str(self.directory_to_store_files))
             self.path_to_start = interface_with_file_manager.get_way()
-       
+
         except BaseException:
-                print("Invalid input of data -")
+            print("Invalid input of data -")
         self.run_code()
         self.final_decision()
-        
 
     def create_extensions_folders(self, needed_extensions):
         self.needed_extensions = needed_extensions
@@ -34,49 +32,37 @@ class FileManager():
             os.makedirs(self.extension_folders_in_mainfolder)
             yield extension
 
-
-
-
-    def create_full_path(self,extension_for_full_path):
+    def create_full_path(self, extension_for_full_path):
         for address, dirs, files in os.walk(self.path_to_start):
 
             for file in files:
 
                 if extension_for_full_path in file:
-                     full_path_to_file = os.path.join(address, file)
-                     if time.time() - os.path.getctime(full_path_to_file) < self.days and not any(
-                         defect in full_path_to_file for defect in list_defect):
+                    full_path_to_file = os.path.join(address, file)
+                    if time.time() - os.path.getctime(full_path_to_file) < self.days and not any(
+                            defect in full_path_to_file for defect in list_defect):
 
                         yield full_path_to_file
-
-
-    
 
     def copy_to_extensions_folders(self, full_path_to_file):
         self.path_to_copy_file = os.path.join(self.directory_to_store_files, self.mainfolder_name)
         for extension in self.list_needed_extensions:
             try:
                 if extension in full_path_to_file:
-                    shutil.copy(
-                        full_path_to_file, os.path.join(
-                            self.path_to_copy_file, extension))
+                    shutil.copy(full_path_to_file, os.path.join(self.path_to_copy_file, extension))
 
             except BaseException:
                 with open(os.path.join(self.path_to_copy_file, "mistakes.txt"), 'a') as file_mistake:
                     file_mistake.write(f"{full_path_to_file}\n")
-    
+
     def run_code(self):
-         for extension in self.create_extensions_folders(self.needed_extensions):
+        for extension in self.create_extensions_folders(self.needed_extensions):
 
             for full_path_to_file in self.create_full_path(extension):
                 self.copy_to_extensions_folders(full_path_to_file)
                 print(f"File is copied {full_path_to_file}")
 
-    
     def final_decision(self):
         self.what_to_do = interface_with_file_manager.get_final_decision()
         if self.what_to_do == "Yes":
             shutil.rmtree(self.mainfolder_name)
-
-
-
